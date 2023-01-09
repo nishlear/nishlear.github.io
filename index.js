677982d8c3bee78693f11efd7fb6094368966ac9
nishlear <nishlear@gmail.com>
2023-01-09 22:26:09 +0700

    done navbar, css later

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ const banner = document.getElementById('banner');
 const div1 = document.getElementById('page1');
 const div2 = document.getElementById('page2');
 const div3 = document.getElementById('page3');
+const item = document.getElementsByClassName('nav-item');
 
-// ==========scrolling handler==========
+// =====fancy nav thinggy=====
+window.onload = item[0].className += " active"
 
+function selectMenuItem(element) {
+    for (let i = 0; i < item.length; i++) {
+        // item[i].classList.remove('active');
+        item[i].className = item[i].className.replace(" active", "");
+    }
+    element.className += " active";
+}
+// ==========scroll handler==========
 // banner
 banner.addEventListener('wheel', (event) => {
     if (event.deltaY > 0) {
         // scrolling down
         event.preventDefault();
         div1.scrollIntoView({behavior: 'smooth'});
+        selectMenuItem(item[1]);
     }
 })
 // div1
@@ -19,10 +30,13 @@ div1.addEventListener('wheel', (event) => {
         // scrolling down
         event.preventDefault();
         div2.scrollIntoView({behavior: 'smooth'});
+        selectMenuItem(item[2]);
     }
     if (event.deltaY < 0) {
+        // scrolling up
         event.preventDefault();
         banner.scrollIntoView({behavior: 'smooth'});
+        selectMenuItem(item[0]);
     }
 });
 // div 2
@@ -31,11 +45,13 @@ div2.addEventListener('wheel', (event) => {
         // scrolling down
         event.preventDefault();
         div3.scrollIntoView({behavior: 'smooth'});
+        selectMenuItem(item[3]);
    }
     if (event.deltaY < 0) {
         // scrolling up
         event.preventDefault();
         div1.scrollIntoView({behavior: 'smooth'});
+        selectMenuItem(item[1]);
     }
 });
 // div3 
@@ -44,5 +60,6 @@ div3.addEventListener('wheel', (event) => {
         // scrolling up
         event.preventDefault();
         div2.scrollIntoView({behavior: 'smooth'});
+        selectMenuItem(item[2]);
     }
 });

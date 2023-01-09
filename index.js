cd7b7596f8e476cf42c5162c1af7d1117125b918
nishlear <nishlear@gmail.com>
2023-01-09 15:22:37 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const div1 = document.getElementById('page1');
+const div2 = document.getElementById('page2');
+const div3 = document.getElementById('page3');
+
+// div1
+div1.addEventListener('wheel', (event) => {
+    if (event.deltaY > 0) {
+        // scrolling down
+        event.preventDefault();
+        div2.scrollIntoView({behavior: 'smooth'});
+    }
+});
+// div 2
+div2.addEventListener('wheel', (event) => {
+    if (event.deltaY > 0) {
+        // scrolling down
+        event.preventDefault();
+        div3.scrollIntoView({behavior: 'smooth'});
+   }
+    if (event.deltaY < 0) {
+        // scrolling up
+        event.preventDefault();
+        div1.scrollIntoView({behavior: 'smooth'});
+    }
+});
+// div3 
+div3.addEventListener('wheel', (event) => {
+    if (event.deltaY < 0) {
+        // scrolling up
+        event.preventDefault();
+        div2.scrollIntoView({behavior: 'smooth'});
+    }
+});

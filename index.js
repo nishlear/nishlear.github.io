7444220327016766bfb985f64c14fe3a85d38018
nishlear <nishlear@gmail.com>
2023-01-09 17:48:07 +0700

    add side navbar

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+const banner = document.getElementById('banner');
 const div1 = document.getElementById('page1');
 const div2 = document.getElementById('page2');
 const div3 = document.getElementById('page3');
 
+// ==========scrolling handler==========
+
+// banner
+banner.addEventListener('wheel', (event) => {
+    if (event.deltaY > 0) {
+        // scrolling down
+        event.preventDefault();
+        div1.scrollIntoView({behavior: 'smooth'});
+    }
+})
 // div1
 div1.addEventListener('wheel', (event) => {
     if (event.deltaY > 0) {
         // scrolling down
         event.preventDefault();
         div2.scrollIntoView({behavior: 'smooth'});
+    }
+    if (event.deltaY < 0) {
+        event.preventDefault();
+        banner.scrollIntoView({behavior: 'smooth'});
     }
 });
 // div 2
